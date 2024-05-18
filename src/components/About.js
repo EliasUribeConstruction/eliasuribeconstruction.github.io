62c4cd7b10feb88eada5142e02f7a12fbf5c4214
Malcolm Weaver <malcolm.weaver12128@gmail.com>
2024-05-18 10:02:43 -0500

    About text section

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,23 @@
 import "../App.css";
-import whiteBluePrint from "../assets/BluePrint.jpg";
-import architectBluePrint from "../assets/ArchitectBluePrint.jpg";
-
+import eliasPhoto from "../assets/EliasPhoto.jpeg";
 function About() {
   return (
-    <section id="about">
-      <div className="about-container">
-        <div className="text-section">
-          {/* Text content goes here */}
-          <div className="about-text">
-            From concept to completion, we've got you covered.
-          </div>
-          <div className="about-title"> ABOUT </div>
+    <section id="about" className="scroll-my-10">
+      <div className="max-w-[80%] mx-auto my-[3rem]">
+        <div className="text-[#0a3863] text-center text-4xl font-semibold font-[Poppins] my-[2rem]">
+          ABOUT
         </div>
-        <div className="image-section">
-          <div className="second-image">
-            <img src={architectBluePrint} alt="Shorter Image" />
-          </div>
-          <div className="first-image">
-            <img src={whiteBluePrint} alt="Longer Image" />
-          </div>
+        <div className="text-[#0a3863] text-center max-w-[80%] mx-auto text-2xl font-semibold font-[Poppins]">
+          From concept to completion, we've got you covered.
         </div>
-      </div>
-      <div>
-        I’ve been a builder in the Bozeman area now for the past couple years.
-        Specializing in remodels, interior finish work, exterior siding and
-        decks. In my free time you can catch me practicing archery or
-        backpacking in the beartooths. Look forward to helping complete your
-        dream projects.
+        <div className=" text-2xl font-[Poppins] text-center py-[3rem] text-blue-950">
+          I’ve been a builder in the Bozeman area now for the past couple years.
+          Specializing in remodels, interior finish work, exterior siding and
+          decks. In my free time you can catch me practicing archery or
+          backpacking in the beartooths. Look forward to helping complete your
+          dream projects.
+        </div>
+        <img src={eliasPhoto} alt="Elias" className="mx-auto"></img>
       </div>
     </section>
   );
