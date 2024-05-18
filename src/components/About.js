20d2c320d885a880e3eec0f02092c4ae45bdc2e7
Malcolm Weaver <malcolm.weaver12128@gmail.com>
2024-05-18 14:52:00 -0500

    Elias asked for these changes.

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,12 @@ function About() {
   return (
     <section id="about" className="scroll-my-10">
       <div className="max-w-[80%] mx-auto my-[3rem]">
-        <div className="text-[#0a3863] text-center text-4xl font-semibold font-[Poppins] my-[2rem]">
+        {/* <div className="text-[#0a3863] text-center text-4xl font-semibold font-[Poppins] my-[2rem]">
           ABOUT
         </div>
         <div className="text-[#0a3863] text-center max-w-[80%] mx-auto text-2xl font-semibold font-[Poppins]">
           From concept to completion, we've got you covered.
-        </div>
+        </div> */}
         <div className=" text-2xl font-[Poppins] text-center py-[3rem] text-blue-950">
           I’ve been a builder in the Bozeman area now for the past couple years.
           Specializing in remodels, interior finish work, exterior siding and
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,51 +1,72 @@
-import '../App.css';
-import Project1 from '../assets/FinishedBuilding1.jpeg'
-import Project2 from '../assets/FinishedBuilding2.jpeg'
-import Project3 from '../assets/FinishedBuilding3.jpeg'
+import "../App.css";
+import Project1 from "../assets/FinishedBuilding1.jpeg";
+import LivingstonRemodel from "../assets/LivingstonRemodel.jpeg";
+import BigSkyModern from "../assets/BigSkyModern.jpeg";
 
 function Portfolio() {
-    return (
-        <section id="portfolio" className="portfolio">
-            <div className="portfolio-title">PORTFOLIO</div>
-            <div className="portfolio-container">
-                <div className="portfolio-item">
-                    <img src={Project1} className="portfolio-image" alt="Project1"/>
-                    <div className="portfolio-text-section">
-                        <div className="project-title">The Bridger Build</div>
-                        <div className="project-quote">
-                            "Elias was prompt, hard-working, and has a positive attitude. He made sure the job was done right. Good attention to detail. I'd definitely hire him again."
-                        </div>
-                        <div className="quote-name">- Jeff Browning | Designer & Ultrarunning Coach</div>
-                    </div>
-                </div>
-                <div className="portfolio-item">
-                    <img src={Project2} className="portfolio-image-switch" alt="Project2"/>
-                    <div className="portfolio-text-section-switch">
-                        <div className="project-title">The Wilson Remodel</div>
-                        <div className="project-quote">
-                            "Elias is profoundly thoughtful in his work. I have never hired or worked with a professional who is equally dedicated, skilled and tireless."
-                        </div>
-                        <div className="quote-name">- John Dolan | MA, DDS, MS</div>
-                    </div>
-                </div>
-                <div className="portfolio-item">
-                    <img src={Project3} className="portfolio-image" alt="Project3"/>
-                    <div className="portfolio-text-section">
-                        <div className="project-title">Big Sky Modern</div>
-                        <div className="project-quote">
-                        "Elias's work was quality, on time, on budget, and with a great attitude. I will work with Elias again!"
-                        </div>
-                        <div className="quote-name">- Seth Bork | Project Management</div>
-                    </div>
-                </div>
+  return (
+    <section id="portfolio" className="portfolio">
+      <div className="portfolio-title">PORTFOLIO</div>
+      <div className="portfolio-container">
+        <div className="portfolio-item">
+          <img src={Project1} className="portfolio-image" alt="Project1" />
+          <div className="portfolio-text-section">
+            <div className="project-title">The Bridger Build</div>
+            <div className="project-quote">
+              "Elias was prompt, hard-working, and has a positive attitude. He
+              made sure the job was done right. Good attention to detail. I'd
+              definitely hire him again."
             </div>
-            <div className="big-quote-section">
-                <div className="big-quote-title">What Our Customers Are Saying</div>
-                <div className="big-quote">"Elias Uribe has worked on several projects for me over the last three years on property in Cottonwood Canyon south of Bozeman. Elias is profoundly thoughtful in his work. I have never hired or worked with a professional who is equally dedicated, skilled and tireless. Elias will bring insight and problem-solving skills to your project. You will find that he is personable with clients and colleagues alike. I can’t recommend Elias highly enough and I look forward to hiring him on my upcoming architectural projects. There is no one I would rather have on my team."</div>
-                <div className="big-quote-name">- John Dolan | MA, DDS, MS</div>
+            <div className="quote-name">
+              - Jeff Browning | Designer & Ultrarunning Coach
             </div>
-        </section> 
-    )
+          </div>
+        </div>
+        <div className="portfolio-item">
+          <img
+            src={LivingstonRemodel}
+            className="portfolio-image-switch"
+            alt="Project2"
+          />
+          <div className="portfolio-text-section-switch">
+            <div className="project-title">Livingston Remodel</div>
+            <div className="project-quote">
+              "Elias is profoundly thoughtful in his work. I have never hired or
+              worked with a professional who is equally dedicated, skilled and
+              tireless."
+            </div>
+            <div className="quote-name">- John Dolan | MA, DDS, MS</div>
+          </div>
+        </div>
+        <div className="portfolio-item">
+          <img src={BigSkyModern} className="portfolio-image" alt="Project3" />
+          <div className="portfolio-text-section">
+            <div className="project-title">Big Sky Modern</div>
+            <div className="project-quote">
+              "Elias's work was quality, on time, on budget, and with a great
+              attitude. I will work with Elias again!"
+            </div>
+            <div className="quote-name">- Seth Bork | Project Management</div>
+          </div>
+        </div>
+      </div>
+      <div className="big-quote-section">
+        <div className="big-quote-title">What Our Customers Are Saying</div>
+        <div className="big-quote">
+          "Elias Uribe has worked on several projects for me over the last three
+          years on property in Cottonwood Canyon south of Bozeman. Elias is
+          profoundly thoughtful in his work. I have never hired or worked with a
+          professional who is equally dedicated, skilled and tireless. Elias
+          will bring insight and problem-solving skills to your project. You
+          will find that he is personable with clients and colleagues alike. I
+          can’t recommend Elias highly enough and I look forward to hiring him
+          on my upcoming architectural projects. There is no one I would rather
+          have on my team."
+        </div>
+        <div className="big-quote-name">- John Dolan | MA, DDS, MS</div>
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
